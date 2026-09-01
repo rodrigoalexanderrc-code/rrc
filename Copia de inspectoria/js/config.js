b3b41e475236e95d45859cb77925d267dbbcf9ae
rodrigoalexanderrc-code <rodrigoalexander.rc@gmail.com>
2026-09-01 15:07:04 -0400

    a

--- Copia de inspectoria/js/config.js
+++
@@ -1,4 +0,0 @@
-// Archivo de configuración global
-const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbyk8iWmgGkL9mkgEKea-9ko0hLiHJWQlAk82jrP07Bdc1DHtH66b2Ke26GsS5aGmKNy/exec"
-};
